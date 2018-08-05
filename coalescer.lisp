4c6b6163ac4cdba9dd611f2c5ab328d0fef8603e
Alexander Gutev <alex.gutev@gmail.com>
2018-08-05 14:22:11 +0200

    Add stricter conditions for input node coalescing.
    Input nodes (nodes with no dependencies) are only coalesced if they
    have a single observer, of which all its dependencies are input nodes
    with a single observer.

--- coalescer.lisp
+++ coalescer.lisp
@@ -20,36 +20,83 @@
 
 (in-package :metalink.frontend)
 
+(in-readtable cut-syntax)
+
+
 (defun coalesce-nodes (graph)
   "Coalesces successive nodes, which only have a single observer, into
    single nodes"
 
   (let ((visited (make-hash-table :test #'eq)))
     (labels
-        ((coalesce-node (node)
+        ((coalesce-input-node (input-node)
+           "Coalesces the input node into its observer node only if:
+            it has a single observer and all the observer's
+            dependencies are input nodes with a single observer."
+
+           (when (every #'input-dependencies? (observer-list input-node))
+             (remove-node input-node)))
+
+         (input-dependencies? (node)
+           "Returns true if all dependencies of NODE are input
+            nodes (have no dependencies) and only have a single
+            observer."
+
+           (every #L(and (zerop (dependencies-count %1)) (= (observers-count %1) 1))
+                  (dependency-list node)))
+
+         (coalesce-observers (node)
+           "Performs node coalescing on the observer nodes of NODE."
+
+           (mapc #'coalesce-node (observer-list node)))
+
+         (coalesce-node (node)
+           "Coalesces the node NODE (into its observer node) if it
+            only has a single observer, after node coalescing is
+            performed on its observer nodes."
+
            (unless (gethash node visited)
              (setf (gethash node visited) t)
 
-             (mapc #'coalesce-node (observer-list node))
+             (coalesce-observers node)
+             (remove-node node)))
 
-             (when (= (observers-count node) 1)
-               (remhash (name node) (nodes graph))
-               (remhash (name node) (all-nodes graph))
+         (remove-node (node)
+           "Removes the node NODE if it has a single observer. Its
+            dependencies are merged into the dependency set of its
+            observer node, it's replaced with its observer node in the
+            observer sets of its dependencies, and the node referenced
+            within the `node-link' objects of its observer nodes, is
+            replaced with the value-function of NODE."
 
-               (let ((observer (first (observer-list node))))
-                 (merge-dependencies node observer)
-                 (replace-operand node observer)))))
+           (when (= (observers-count node) 1)
+             (remhash (name node) (nodes graph))
+             (remhash (name node) (all-nodes graph))
+
+             (let ((observer (first (observer-list node))))
+               (merge-dependencies node observer)
+               (replace-operand node observer))))
 
          (replace-operand (operand node)
+           "Replaces the node OPERAND, within the `node-link' objects
+            corresponding to OPERAND within the dependency set of
+            NODE, with the value function of OPERAND."
+
            (with-slots (dependencies) node
              (mapc (rcurry #'replace-link-node (or (value-function operand)
                                                    (name operand)))
                    (ensure-list (gethash node (observers operand))))))
 
          (replace-link-node (link replacement)
+           "Replaces the node with the `node-link' LINK with
+            REPLACEMENT."
            (setf (node-link-node link) replacement))
 
          (merge-dependencies (dependency observer)
+           "Merges the dependencies of DEPENDENCY into the dependency
+            set of OBSERVER. DEPENDENCY is removed from the dependency
+            set of OBSERVER."
+
            (with-slots (dependencies) observer
              (remhash dependency dependencies)
 
@@ -67,7 +114,10 @@
 
                  (remhash dependency observers))))))
 
-      (mapc #'coalesce-node (input-nodes graph))
+      (let ((inputs (input-nodes graph)))
+        (mapc #'coalesce-observers inputs)
+        (mapc #'coalesce-input-node inputs))
+
       (maphash-values (compose #'coalesce-nodes #'definition) (meta-nodes graph))
       (maphash-values #'coalesce-node-links (all-nodes graph)))))
 
@@ -81,6 +131,13 @@
 
   (with-slots (dependencies observers value-function) node
     (labels ((remove-node-links (fn)
+               "Replaces all `node-link' objects (within the value
+                function FN), which do not directly reference another
+                node, with their contents. All `node-link' objects
+                which directly reference the same node are replaced
+                with a single `node-link' object referencing that
+                node."
+
                (match fn
                  ((list* meta-node operands)
                   (list* meta-node (mapcar #'remove-node-links operands)))
@@ -91,13 +148,18 @@
                       (node
                        (gethash node dependencies))
 
-                      (symbol fn)
+                      (symbol
+                       fn)
 
                       (otherwise (remove-node-links node)))))
 
                  (_ fn)))
 
              (make-new-node-link (dependency)
+               "Creates a new `node-link' object for the dependency
+                node DEPENDENCY and adds it to the dependencies table
+                of NODE."
+
                (alet (setf (gethash dependency dependencies) (node-link dependency))
                  (setf (gethash node (observers dependency)) it))))
 
