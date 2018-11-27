c5d162aededd74d01b4585f35282a52f3f101f6c
Alexander Gutev <alex.gutev@gmail.com>
2018-11-27 08:24:18 +0100

    Refactor printing of JavaScript AST nodes.

--- backends/javascript/print.lisp
+++ backends/javascript/print.lisp
@@ -62,66 +62,66 @@
   "Prints the JavaScript code represented by each AST node, in
    AST-NODES, to *STANDARD-OUTPUT*."
 
-  (print-ast ast-nodes t)
+  (print-ast ast-nodes :semicolon t)
   nil)
 
 
-(defgeneric print-ast (ast-node &optional semicolon brackets)
-  (:documentation
-   "Prints the JavaScript code represent by AST-NODE. If SEMICOLON is
-    true, the code is followed by a semicolon. If BRACKETS is true the
-    code is enclosed in brackets (... code ...)."))
+;;; Printing AST nodes
 
-(defmethod print-ast ((ast-nodes list) &optional semicolon brackets)
+(defgeneric print-ast (ast-node &key &allow-other-keys)
+  (:documentation
+   "Prints the JavaScript code represent by AST-NODE. If :SEMICOLON is
+    true, the code is followed by a semicolon. If :BRACKETS is true
+    the code is enclosed in brackets (... code ...)."))
+
+(defmethod print-ast ((ast-nodes list) &key semicolon brackets)
   "Prints each AST node in AST-NODES."
 
-  (mapc (rcurry #'print-ast semicolon brackets) ast-nodes))
+  (mapc (rcurry #'print-ast :semicolon semicolon :brackets brackets) ast-nodes))
 
-(defmethod print-ast ((ast-nodes array) &optional semicolon brackets)
+(defmethod print-ast ((ast-nodes array) &key semicolon brackets)
   "Prints each AST node in AST-NODES if AST-NODES is not a string."
 
   (if (stringp ast-nodes)
       (call-next-method ast-nodes)
-      (map nil (rcurry #'print-ast semicolon brackets) ast-nodes)))
+      (map nil (rcurry #'print-ast :semicolon semicolon :brackets brackets) ast-nodes)))
+
 
 ;;; Expressions
 
-(defmethod print-ast ((expression js-new) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((expression js-new) &key)
   (print-token "new" :space t)
   (call-next-method))
 
-(defmethod print-ast ((expression js-element) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((expression js-element) &key)
   (with-accessors ((object js-element-object)
                    (element js-element-element))
       expression
+
     (print-ast object)
     (print-token "[" :lead-space nil :trail-space nil)
     (print-ast element)
     (print-token "]" :lead-space nil)))
 
-(defmethod print-ast ((expression js-member) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((expression js-member) &key)
   (with-accessors ((object js-member-object)
                    (field js-member-field))
       expression
+
     (print-ast object)
     (print-token "." :lead-space nil :trail-space nil)
     (print-ast field)))
 
-(defmethod print-ast ((array js-array) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((array js-array) &key)
   (print-token "[" :trail-space nil)
   (print-ast-list (js-array-elements array))
   (print-token "]" :lead-space nil))
 
 
 (defun print-ast-list (expressions &optional brackets)
+  "Prints a list of comma-separated JS expressions. If BRACKETS is
+   true the entire list of expressions is surrounded in brackets."
+
   (when brackets
     (print-token "(" :lead-space nil :trail-space nil))
 
@@ -136,9 +136,7 @@
   (when brackets
     (print-token ")" :lead-space nil)))
 
-(defmethod print-ast ((object js-object) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((object js-object) &key)
   (print-token "{")
   (print-newline)
 
@@ -164,9 +162,7 @@
 
 ;;; Operator/Function call expressions
 
-(defmethod print-ast ((expression js-call) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((expression js-call) &key)
   (with-accessors ((operator js-call-operator)
                    (operands js-call-operands))
       expression
@@ -176,72 +172,70 @@
         ((and (= num-args 2)
               (member operator +js-binary-operators+))
          (print-binary-expression operator operands))
+
         ((and (= num-args 1)
               (member operator +js-unary-operators+))
          (print-unary-expression operator operands))
+
         (t
          (print-function-call operator operands))))))
 
 (defun print-function-call (function arguments)
-  (print-ast function nil (typep function 'js-function))
+  (print-ast function
+             :semicolon nil
+             :brackets (or (js-call-p function)
+                           (js-function-p function)))
   (print-ast-list arguments t))
 
 (defun print-binary-expression (operator operands)
   (destructuring-bind (left right) operands
-    (print-ast left nil (js-call-p left))
+    (print-ast left :semicolon nil :brackets (js-call-p left))
     (print-token operator)
-    (print-ast right nil (js-call-p right))))
+    (print-ast right :semicolon nil :brackets (js-call-p right))))
 
 (defun print-unary-expression (operator operands)
   (print-token operator)
-  (print-ast (first operands) nil t))
+  (print-ast (first operands) :semicolon nil :brackets t))
 
 
 ;;;; Blocks
 
-(defmethod print-ast ((block js-if) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((block js-if) &key)
   (with-accessors ((condition js-if-condition)
                    (then js-if-then)
                    (else js-if-else))
       block
+
     (print-token "if")
-    (print-ast condition nil t)
-    (print-ast then else)
+    (print-ast condition :semicolon nil :brackets t)
+    (print-ast then :semicolon else)
 
     (when else
       (print-newline)
       (print-token "else" :space t)
       (print-ast else))))
 
-(defmethod print-ast ((block js-block) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((block js-block) &key)
   (print-token "{")
   (print-newline)
 
   (let ((*indent-level* (1+ *indent-level*)))
-    (map nil (rcurry #'print-ast t) (js-block-statements block)))
+    (map nil (rcurry #'print-ast :semicolon t) (js-block-statements block)))
 
   (print-newline)
   (print-token "}"))
 
 
-(defmethod print-ast ((while js-while) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((while js-while) &key)
   (with-accessors ((condition js-while-condition)
                    (body js-while-body))
       while
 
     (print-token "while")
-    (print-ast condition nil t)
+    (print-ast condition :semicolon nil :brackets t)
     (print-ast body)))
 
-(defmethod print-ast ((func js-function) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
-
+(defmethod print-ast ((func js-function) &key)
   (with-accessors ((name js-function-name)
                    (args js-function-arguments)) func
 
@@ -254,18 +248,16 @@
     (call-next-method)))
 
 
-
 ;;; Statements
 
-(defmethod print-ast ((ret js-return) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast ((ret js-return) &key)
   (print-token "return" :space t)
   (print-ast (js-return-value ret)))
 
-(defmethod print-ast ((var js-var) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast ((var js-var) &key)
   (with-accessors ((name js-var-var)
                    (value js-var-value)) var
+
     (print-token "var" :space t)
     (print-token name)
 
@@ -273,42 +265,35 @@
       (print-token "=")
       (print-ast value))))
 
-(defmethod print-ast ((continue js-continue) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast ((continue js-continue) &key)
   (print-token "continue"))
 
-(defmethod print-ast ((throw js-throw) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast ((throw js-throw) &key)
   (print-token "throw")
   (print-ast (js-throw-expression throw)))
 
 
 ;;; Other expressions
 
-(defmethod print-ast ((thing null) &optional semicolon brackets)
-  (declare (ignore semicolon brackets)))
+(defmethod print-ast ((thing null) &key))
 
-(defmethod print-ast (thing &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast (thing &key)
   (print-token thing))
 
-(defmethod print-ast ((thing js-string) &optional semicolon brackets)
-  (declare (ignore semicolon brackets))
+(defmethod print-ast ((thing js-string) &key)
   (print-token (format nil "~s" (js-string-string thing))))
 
 
 ;;; Semicolons and bracketing
 
-(defmethod print-ast :before ((thing t) &optional semicolon brackets)
-  (declare (ignore semicolon))
-
+(defmethod print-ast :before ((thing t) &key brackets)
   (unless (listp thing)
     (print-leading-space))
 
   (when brackets
     (print-token "(" :trail-space nil)))
 
-(defmethod print-ast :after ((thing t) &optional semicolon brackets)
+(defmethod print-ast :after ((thing t) &key semicolon brackets)
   (when brackets
     (print-token ")" :lead-space nil))
 
@@ -321,13 +306,17 @@
 ;;;; Utility functions
 
 (defun print-token (token &key space (lead-space t) (trail-space t))
-  "Prints TOKEN preceded with the necessary whitespace. If the keyword
-   argument :SPACE is true, a space is printed after the token
-   regardless of the current value of *PRINT-INDENTED* or
-   *PRINT-SPACE*, by default is is NIL. If :LEAD-SPACE is NIL, no
-   whitespace will be printed before the token if :TRAIL-SPACE is NIL,
-   *PRINT-SPACE* is NIL the next token printed will not be preceded by
-   a space unless a newline was printed after this token."
+  "Prints TOKEN preceded with the necessary whitespace.
+
+   If the keyword argument :SPACE is true, a space is printed after
+   the token regardless of the current value of *PRINT-INDENTED* or
+   *PRINT-SPACE*.
+
+   If :LEAD-SPACE is NIL no whitespace will be printed before the
+   token.
+
+   If :TRAIL-SPACE is NIL the next token printed will not be preceded
+   by a space unless a newline was printed after this token."
 
   (let ((*print-space* (and *print-space* lead-space)))
     (print-leading-space)
@@ -351,9 +340,11 @@
     (cond
       (*print-newline*
        (fresh-line)
+
        (loop repeat (* *indent-level* *indent-offset*)
           do
             (princ " "))
+
        (setf *print-newline* nil)
        (setf *print-space* nil))
 
